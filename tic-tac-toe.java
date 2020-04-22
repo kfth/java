aa2aefceacea543fda1f36595a41a327500f49d0
kfth <nieuwleven33@gmail.com>
2020-04-22 23:40:56 +0200

    tic-tac-toe

---
+++ tic-tac-toe.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.print("O ");
+        System.out.print("X ");
+        System.out.println("X");
+        System.out.print("O ");
+        System.out.print("X ");
+        System.out.println("O");
+        System.out.print("X ");
+        System.out.print("O ");
+        System.out.print("X");
+    }
+}
